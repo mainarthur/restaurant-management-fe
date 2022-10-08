cd2ec9d34c915d6be213bac68a457cea82ab0caf
Arthur Kh <listbox6@gmail.com>
2022-10-08 18:59:26 +0300

    feat: antd theming setup + added main deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import vitePluginImp from "vite-plugin-imp";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    vitePluginImp({
+      libList: [
+        {
+          libName: "antd",
+          style: (name) => `antd/es/${name}/style`,
+        },
+      ],
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          "primary-color ": "#323cf0",
+          "border-color-base": "#848484",
+        },
+        javascriptEnabled: true,
+      },
+    },
+  },
+});
