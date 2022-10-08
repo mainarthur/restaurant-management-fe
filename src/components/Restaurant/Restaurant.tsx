4a68dbb28080d1ebd079b784c6b7ed2ec7f1bfbc
Arthur Kh <listbox6@gmail.com>
2022-10-08 21:37:23 +0300

    feat: updated styles for restaurant component

--- src/components/Restaurant/Restaurant.tsx
+++ src/components/Restaurant/Restaurant.tsx
@@ -1,1 +1,101 @@
-export const Restaurant = () => {};
+import styled from "@emotion/styled";
+import { Button, Card, Space, Typography } from "antd";
+
+import { Restaurant as RestaurantType } from "../../redux/types/Restaurant.js";
+
+type Handler = (id: number) => void;
+type Props = RestaurantType & { onUpdate: Handler; onDelete: Handler };
+
+export const Restaurant = ({ id, name, address, email, phone }: Props) => (
+  <RestaurantContainer>
+    <ContactContainer>
+      <span>
+        <Title>{name}</Title>
+        <Address>{address}</Address>
+      </span>
+      <DeleteButton>
+        <MinusOutlined />
+      </DeleteButton>
+    </ContactContainer>
+    <ContactContainer>
+      <Email>{email}</Email>
+      <Phone>{phone}</Phone>
+    </ContactContainer>
+  </RestaurantContainer>
+);
+
+const Title = styled(Typography)`
+  font-size: 22pt;
+  font-weight: bold;
+`;
+
+const Address = styled(Typography)`
+  font-size: 16pt;
+`;
+
+const Email = styled(Typography)`
+  font-size: 16pt;
+  color: #323cf0;
+`;
+
+const Phone = Address;
+
+const DeleteButton = styled(Button)`
+  width: 44pt;
+  height: 44pt;
+  background-color: #4c54ee;
+  color: white;
+  font-weight: bold;
+
+  &:hover {
+    background-color: #4c54ee;
+  }
+  &:active {
+    background-color: #4c54ee;
+  }
+  &:focus {
+    background-color: #4c54ee;
+  }
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  display: none;
+`;
+
+const RestaurantContainer = styled.div`
+  width: 750pt;
+  height: 134pt;
+  background-color: #f3f4f5;
+  margin-bottom: 10pt;
+  padding-top: 18pt;
+  padding-left: 20pt;
+  padding-right: 20pt;
+  padding-bottom: 12pt;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  &:hover {
+    background-color: #323cf0;
+    article {
+      color: white;
+    }
+    button {
+      display: flex;
+    }
+  }
+`;
+
+const ContactContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+const MinusOutlined = styled.div`
+  width: 12pt;
+  height: 2pt;
+  background-color: white;
+`;
--- src/components/RestaurantsList/RestaurantsList.tsx
+++ src/components/RestaurantsList/RestaurantsList.tsx
@@ -1,1 +1,66 @@
-export const RestaurantsList = () => {};
+import { PlusOutlined } from "@ant-design/icons";
+import styled from "@emotion/styled";
+import { Button, Card, Pagination, Space, Typography } from "antd";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { useEvent } from "../../hooks/useEvent.js";
+import {
+  deleteRestaurantActionAsync,
+  searchActionAsync,
+} from "../../redux/actions/restaurants.js";
+import { createAsyncAction } from "../../redux/helpers.js";
+import { RootState } from "../../redux/reducers";
+import { Restaurant } from "../Restaurant/Restaurant.js";
+
+export const RestaurantsList = () => {
+  const [updatingRestaurantId, setUpdatingRestaurantId] = useState<
+    number | undefined
+  >();
+
+  const restaurants = useSelector((state: RootState) => state.restaurants);
+  const pagination = useSelector((state: RootState) => state.pagination);
+  const dispatch = useDispatch();
+
+  const handleDeleteRestaurant = useEvent(async (id: number) => {
+    await createAsyncAction(dispatch, deleteRestaurantActionAsync({ id }));
+    await createAsyncAction(
+      dispatch,
+      searchActionAsync({
+        searchTerm: pagination.searchTerm,
+        page: restaurants.length === 1 ? pagination.page - 1 : pagination.page,
+        pageSize: pagination.pageSize,
+      }),
+    );
+  });
+  const handleUpdateRestaurant = useEvent((id: number) => {
+    setUpdatingRestaurantId(id);
+  });
+
+  return (
+    <RestaurantsListWrapper>
+      {restaurants.map(({ id, name, address, email, phone }) => (
+        <Restaurant
+          key={id}
+          id={id}
+          name={name}
+          address={address}
+          email={email}
+          phone={phone}
+          onDelete={handleDeleteRestaurant}
+          onUpdate={handleUpdateRestaurant}
+        />
+      ))}
+      <Space>
+        <Button type="primary">
+          <PlusOutlined />
+        </Button>
+        <Pagination />
+      </Space>
+    </RestaurantsListWrapper>
+  );
+};
+
+const RestaurantsListWrapper = styled.div`
+  margin-top: 42pt;
+`;
